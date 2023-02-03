e4b6133c5b757c2cfd7d06e2a8e9ac7dc12652ed
Aluel-kuot <maryaluelkuot@gmail.com>
2023-02-03 11:51:38 +0300

    biology

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    var Name= ("Alice")
+    println(Name)
+    var subject=("history")
+    println(subject)
+    var Marks=("90")
+    println(Marks)
+    var grade=("A-")
+    println(grade)
+
+}
